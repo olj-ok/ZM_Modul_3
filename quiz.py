b65f55d848b02418ba6049539fe0c1aa346ce792
Volodymyr Miroshnychenko <mv.design@gmail.com>
2025-03-28 20:57:54 +0100

    Index entfernt, generator, neues design fur ergebnis

--- quiz.py
+++ quiz.py
@@ -49,10 +49,9 @@ quiz_fragen = (
 
 class Quiz():
 
-    def __init__(self, quiz_fragen):
-        self.punkte      = 0
-        self.frage_index = -1
-        self.fragen = quiz_fragen
+    def __init__(self, fragen):
+        self.fragen         = fragen
+        self.frage_aktuell  = None
         
         self.init_gui()
 
@@ -69,13 +68,11 @@ class Quiz():
         self.frame_buttons.pack(pady=20)
 
     def überprüfe_antwort(self, auswahl):        
-        richtige_antwort = self.fragen[self.frage_index]["antwort"]
-        
-        if auswahl == richtige_antwort:
+        if auswahl == self.frage_aktuell["antwort"]:
             self.punkte += 1
-            messagebox.showinfo("Ergebnis", "✅ Richtig!")
+            self.showinfo("Ergebnis", "Richtig!")
         else:
-            messagebox.showerror("Ergebnis", f"❌ Falsch! Richtige Antwort: {richtige_antwort}")
+            self.showerror("Ergebnis", f"Falsch! Richtige Antwort: {self.frage_aktuell["antwort"]}")
             
         self.nexte_frage()
         
@@ -86,33 +83,47 @@ class Quiz():
     def add_buttons(self, optionen):
         for option in optionen:
             antwort_char = option.split(")")[0]
-            btn = ttk.Button(self.frame_buttons, text=option, width=40, command=lambda a = antwort_char: self.überprüfe_antwort(a))
+            
+            btn = tk.Button(self.frame_buttons, text=option, height=2, width=40, anchor="w", command=lambda a=antwort_char: self.überprüfe_antwort(a))
             btn.pack(pady=5)
 
+    def showinfo(self, title, message):        
+        messagebox.showinfo(title, message, master=self.root)
+
+    def showerror(self, title, message):        
+        messagebox.showerror(title, message, master=self.root)
+            
     def result(self):
-        messagebox.showinfo("Quiz beendet", f"Dein Punktestand: {self.punkte}/{len(self.fragen)}")        
+        
+        self.frage_label.config(text="Quiz beendet")        
+        self.clear_buttons()
+
+        # 90% - grün, sonst rot        
+        farbe = "green" if self.punkte / len(self.fragen) > 0.9 else "red"
+        
+        tk.Label(self.frame_buttons, text=f"Dein Punktestand: {self.punkte}/{len(self.fragen)}", font=("Arial", 12), fg=farbe).pack()
+        
+        tk.Button(self.frame_buttons, text="Neuer Test", width=20, command=self.new_test).pack(pady=(20, 0))
+        tk.Button(self.frame_buttons, text="Beenden", width=20, command=self.root.quit).pack(pady=(10, 0))
                 
-        if messagebox.askyesno("Noch einmal?", "Möchtest du neue Test machen?"):
-            self.new_test()
-        else:
-            self.root.quit()
+    def frage_generator(self):
+        for frage in self.fragen:
+            yield frage
             
     def nexte_frage(self):
-        self.frage_index += 1
         
-        if self.frage_index == len(self.fragen):
+        try:            
+            self.frage_aktuell = next(self.frage)
+            self.frage_label.config(text=self.frage_aktuell["frage"])
+            self.clear_buttons()
+            self.add_buttons(self.frage_aktuell["optionen"])
+        except StopIteration:
             self.result()
             return
-        
-        frage = self.fragen[self.frage_index]        
-        self.frage_label.config(text=frage["frage"])
-        
-        self.clear_buttons()
-        self.add_buttons(frage["optionen"])
 
-    def new_test(self):
-        self.frage_index = -1
-        self.punkte      = 0
+    def new_test(self):        
+        self.punkte = 0
+        self.frage  = self.frage_generator()        
         self.nexte_frage()
 
     def start(self):
